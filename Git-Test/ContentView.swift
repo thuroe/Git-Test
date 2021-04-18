76b7fdf8f3a13a7262a0b13ebef5e6c400fba5c7
Lars Christiansen <larschr@free.fr>
2021-04-18 12:29:57 +0200

    Initial Commit

---
+++ Git-Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git-Test
+//
+//  Created by lars christiansen on 18/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git-Test/Git_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_TestApp.swift
+//  Git-Test
+//
+//  Created by lars christiansen on 18/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Git_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
